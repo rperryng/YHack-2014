5c795c9d8f6652f619471ea44b42f6a3484eb6e7
christopherciufo <christopher.ciufo@gmail.com>
2014-11-01 14:51:20 -0400

    Tessel file now snaps picture then makes Yo request.

--- entry.js
+++ entry.js
@@ -2,11 +2,22 @@ var tessel = require('tessel');
 var wifi = require('wifi-cc3000');
 var needle = require('needle');
 
-var rfidlib = require('rfid-pn532');
-var rfid = rfidlib.use(tessel.port['A']);
+
+// CAMERA
+
+process.env.TESSEL_UPLOAD_DIR = './pictures';
+var camera = require('camera-vc0706').use(tessel.port['D']);
+camera.on('ready', function() {
+  console.log('camera ready...');
+});
+
+
+// RFID
+
+var rfid = require('rfid-pn532').use(tessel.port['A']);
 
 rfid.on('ready', function (version) {
-  console.log('Ready...');
+  console.log('rfid ready...');
 
   rfid.on('data', function (card) {
     console.log(card);
@@ -25,7 +36,37 @@ rfid.on('ready', function (version) {
     } else {
       console.log('not connected');
     }
+    console.log(card.uid.toString('hex'));
 
+    camera.takePicture(function(err, image) {
+
+      if (err) throw err;
+      else {
+        var name = 'IMG_' + Date.now() + '.jpg';
+        process.sendfile(name, image);
+        console.log('snap!');
+        sendYo();
+      }
+
+    });
   });
 
 });
+
+function sendYo () {
+  if (wifi.isConnected()) {
+    var data = {
+      api_token: 'hmm ... this shouldn\'t be here',
+      username: 'christopherciufo'
+    };
+
+    needle
+      .post('https://api.justyo.co/yo/', data)
+      .on('end', function() {
+        console.log('done');
+      });
+  } else {
+    console.log('not connected');
+  }
+}
+
