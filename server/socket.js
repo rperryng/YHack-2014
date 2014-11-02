b4d09caa7ee69eae33f659017b0178a33fb85b55
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-11-02 03:53:53 -0500

    Added timeout for socket

--- server/socket.js
+++ server/socket.js
@@ -10,15 +10,21 @@ module.exports = service;
 var port = 8000;
 
 function connectToSocket() {
-  // INSERT TESSEL IP ADDRESS HERE. Always prepend with 'ws://' to indicate websocket
-  var connection = ws.connect('ws://172.26.1.213:' + port, function () {
-    // When we connect to the server, send some catchy text
-    connection.sendText("My milkshake brings all the boys to the yard");
-  });
 
-  // When we get text back
-  connection.on('text', function (text) {
-    // print it out
-    console.log("Echoed back from tessel:", text);
-  });
+  setTimeout(function () {
+
+    console.log('okay les do dis');
+
+    // INSERT TESSEL IP ADDRESS HERE. Always prepend with 'ws://' to indicate websocket
+    var connection = ws.connect('ws://172.26.1.213:' + port, function () {
+      // When we connect to the server, send some catchy text
+      connection.sendText("My milkshake brings all the boys to the yard");
+    });
+
+    // When we get text back
+    connection.on('text', function (text) {
+      // print it out
+      console.log("Echoed back from tessel:", text);
+    });
+  }, 30000);
 }
